dee696eccd1b580dcd379840477e6a85290da98f
Amauri Blanco Correa <blancoamauri@gmail.com>
2021-12-15 07:30:28 -0300

    1-5 Controlando Offcanvas com Javascript

---
+++ offcanvas e toasts/offcanvas/offcanvas.js
@@ -0,0 +1,11 @@
+var myOffcanvas = document.getElementById('myOffcanvas');
+function show() {
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas)
+    bsOffcanvas.show()
+};
+
+var myOffcanvasStart = document.getElementById('myOffcanvasStart');
+function showStart() {
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvasStart)
+    bsOffcanvas.show()
+};
